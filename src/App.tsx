90f6e396c23b3b1a15d0dd62ac22bd41983434f9
JustasGecevicius <justas.gece@gmail.com>
2023-07-02 15:35:51 +0200

    trying to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { getFirebaseConfig } from './config/firebaseConfig';
-import Header from './components/Header';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import { Projects } from './pages/Projects';
+// import Header from './components/Header';
+// import { Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import { Projects } from './pages/Projects';
 
 function App() {
   const config = getFirebaseConfig();
@@ -13,13 +13,14 @@ function App() {
   getFirestore(app);
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes location={location} key={location.pathname}>
-        <Route path='/' element={<Home />} />
-        <Route path='/Projects' element={<Projects />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='w-screen h-screen bg-red'></div>
+    // <BrowserRouter>
+    //   <Header />
+    //   <Routes location={location} key={location.pathname}>
+    //     <Route path='/' element={<Home />} />
+    //     <Route path='/Projects' element={<Projects />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  server: {
-    host: true,
-  },
+export default defineConfig(({ command }) => {
+  const config = { plugins: [react()], base: '/' };
+
+  if (command !== 'serve') {
+    config.base = '/Portfolio/';
+  }
+
+  return config;
 });
