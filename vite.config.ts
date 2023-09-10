7a3c683dbc505c4dad2d3a583351a7eea7374649
JustasGecevicius <justas.gece@gmail.com>
2023-09-10 14:38:32 +0200

    vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  const config = { plugins: [react()], base: '/' };
-
-  if (command !== 'serve') {
-    config.base = '/Portfolio/';
-  }
+  const config = { plugins: [react()] };
 
   return config;
 });
