d394a1e7c7a0fde2078c2ad010f7f992ec6a326f
JustasGecevicius <justas.gece@gmail.com>
2025-09-11 11:41:52 -0700

    fix SEO points from Lighthouse #2

--- src/components/MainProject.tsx
+++ src/components/MainProject.tsx
@@ -1,6 +1,6 @@
-import { useProjectImages, useProjectText } from '../hooks/hooks';
-import { Firestore } from 'firebase/firestore/lite';
-import githubSVG from '../assets/github-mark-white.svg';
+import { useProjectImages, useProjectText } from "../hooks/hooks";
+import { Firestore } from "firebase/firestore/lite";
+import githubSVG from "../assets/github-mark-white.svg";
 
 export interface MainProjectType {
   project: string;
@@ -14,40 +14,46 @@ export default function MainProject({ project, db }: MainProjectType) {
   return (
     projectImages &&
     projectText && (
-      <div className='flex flex-col items-center pt-4 h-fit md:flex-row md:gap-x-8 md:pt-10'>
+      <div className="flex flex-col items-center pt-4 h-fit md:flex-row md:gap-x-8 md:pt-10">
         <img
           src={projectImages[0]}
-          alt='Project Image'
-          className='rounded-lg md:grow md:basis-0 md:h-full md:w-48'
+          alt="Project Image"
+          className="rounded-lg md:grow md:basis-0 md:h-full md:w-48"
         />
-        <div className='pt-2 h-fit md:grow md:basis-0 md:pt-0'>
-          <h3 className='pt-4 text-2xl text-center md:text-3xl'>{projectText['name']}</h3>
-          <div className='pt-2 h-fit md:pt-4'>
-            <p className='text-justify md:text-xl'>{projectText['p']}</p>
-            <h4 className='pt-2 text-lg font-semibold text-center md:text-xl md:pt-4'>
+        <div className="pt-2 h-fit md:grow md:basis-0 md:pt-0">
+          <h3 className="pt-4 text-2xl text-center md:text-3xl">{projectText["name"]}</h3>
+          <div className="pt-2 h-fit md:pt-4">
+            <p className="text-justify md:text-xl">{projectText["p"]}</p>
+            <h4 className="pt-2 text-lg font-semibold text-center md:text-xl md:pt-4">
               Technologies Used
             </h4>
-            <ul className='pt-2 text-center md:pt-4'>
-              {projectText['tech'].map((elem, index) => {
+            <ul className="pt-2 text-center md:pt-4">
+              {projectText["tech"].map((elem, index) => {
                 return (
-                  <li key={index} className='italic md:text-lg'>
+                  <li key={index} className="italic md:text-lg">
                     {elem}
                   </li>
                 );
               })}
             </ul>
           </div>
-          <div className='flex flex-row justify-center pt-2 gap-x-2 md:pt-4'>
-            <a href={projectText['link']} target='_b' className='h-full'>
-              <button className='h-12 text-lg md:text-xl'>Visit Website</button>
+          <div className="flex flex-row justify-center pt-2 gap-x-2 md:pt-4">
+            <a
+              href={projectText["link"]}
+              target="_b"
+              className="h-12 text-lg md:text-xl text-blue hover:text-light_blue flex-center"
+            >
+              Visit Website
             </a>
-            <a href={projectText['linkGithub']} target='_b' className='h-full'>
-              <button className='h-12 p-0 px-2 hover:scale-125'>
-                <img src={githubSVG} alt='zeba' className='h-2/3 aspect-square' />
-              </button>
+            <a
+              href={projectText["linkGithub"]}
+              target="_b"
+              className="h-12 p-0 px-2 hover:scale-125 flex-center"
+            >
+              <img src={githubSVG} alt="github-image" className="h-2/3 aspect-square" />
             </a>
           </div>
-          <p className='w-full mt-4 mb-2 border-b-2 border-white border-solid rounded-full md:hidden'></p>
+          <p className="w-full mt-4 mb-2 border-b-2 border-white border-solid rounded-full md:hidden"></p>
         </div>
       </div>
     )
--- src/components/ProjectModal.tsx
+++ src/components/ProjectModal.tsx
@@ -1,5 +1,5 @@
-import { ProjectTextType } from '../hooks/hooks';
-import githubSVG from '../assets/github-mark-white.svg';
+import { ProjectTextType } from "../hooks/hooks";
+import githubSVG from "../assets/github-mark-white.svg";
 
 interface ProjectModaltype {
   title: string;
@@ -11,48 +11,49 @@ interface ProjectModaltype {
 
 export default function ProjectModal({ setActive, images, text }: ProjectModaltype) {
   return (
-    <div className='fixed top-0 bottom-0 z-30 flex justify-center w-screen px-5 pt-8'>
-      <div className='fixed top-0 bottom-0 w-screen -z-10 bg-neutral-800'></div>
+    <div className="fixed top-0 bottom-0 z-30 flex justify-center w-screen px-5 pt-8">
+      <div className="fixed top-0 bottom-0 w-screen -z-10 bg-neutral-800"></div>
       <p
-        className='fixed hover:ring-2 hover:ring-[#00aeff] top-0 right-0 flex items-center justify-center w-6 m-2 antialiased text-center text-[#00aeff] bg-white rounded-full md:text-2xl aspect-square md:w-10'
+        className="fixed hover:ring-2 hover:ring-[#00aeff] top-0 right-0 flex items-center justify-center w-6 m-2 antialiased text-center text-[#00aeff] bg-white rounded-full md:text-2xl aspect-square md:w-10"
         onClick={() => {
           setActive(false);
-        }}>
+        }}
+      >
         &#10005;
       </p>
-      <div className='flex flex-col items-center w-screen gap-10 md:scale-150 md:flex-row md:max-w-2xl'>
-        <div className='flex flex-col items-center w-full gap-5 p-1 overflow-y-auto basis-0 aspect-square grow rounded-xl h-fit'>
+      <div className="flex flex-col items-center w-screen gap-10 md:scale-150 md:flex-row md:max-w-2xl">
+        <div className="flex flex-col items-center w-full gap-5 p-1 overflow-y-auto basis-0 aspect-square grow rounded-xl h-fit">
           {Object.values(images).map((elem, index) => {
             return (
               <img
                 key={index}
-                className='h-full aspect-square rounded-xl ring-[#00aeff] ring-2'
+                className="h-full aspect-square rounded-xl ring-[#00aeff] ring-2"
                 src={elem}
-                alt='ProjectImage'
+                alt="ProjectImage"
               />
             );
           })}
         </div>
-        <div className='overflow-y-auto basis-0 grow'>
-          <h3 className='p-2 text-xl font-bold text-center'>{text['name']}</h3>
-          <p className=''>{text['p']}</p>
-          <h4 className='p-2 text-center'>Technologies Used</h4>
-          <ul className='p-2 text-center'>
-            {text['tech'].map((elem, index) => {
+        <div className="overflow-y-auto basis-0 grow">
+          <h3 className="p-2 text-xl font-bold text-center">{text["name"]}</h3>
+          <p className="">{text["p"]}</p>
+          <h4 className="p-2 text-center">Technologies Used</h4>
+          <ul className="p-2 text-center">
+            {text["tech"].map((elem, index) => {
               return (
-                <li key={index} className='italic'>
+                <li key={index} className="italic">
                   {elem}
                 </li>
               );
             })}
           </ul>
-          <div className='flex flex-row justify-center pt-2 mb-2 overflow-hidden h-14 gap-x-2'>
-            <a href={text['link']} target='_b' className='h-full'>
-              <button className='h-full'>Visit Website</button>
+          <div className="flex flex-row justify-center pt-2 mb-2 overflow-hidden h-14 gap-x-2">
+            <a href={text["link"]} target="_b" className="h-full">
+              <button className="h-full">Visit Website</button>
             </a>
-            <a href={text['linkGithub']} target='_b' className='h-full'>
-              <button className='h-full p-0 px-2'>
-                <img src={githubSVG} alt='zeba' className='h-2/3 aspect-square' />
+            <a href={text["linkGithub"]} target="_b" className="h-full">
+              <button className="h-full p-0 px-2">
+                <img src={githubSVG} alt="githuib-image" className="h-2/3 aspect-square" />
               </button>
             </a>
           </div>
